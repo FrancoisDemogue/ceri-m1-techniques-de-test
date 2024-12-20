9d53f20e4304fd578e6df0cb10d76593baa09909
Francois <147613612+FrancoisDemogue@users.noreply.github.com>
2024-12-20 13:28:56 +0100

    Update IPokemonFactory.java

--- src/main/java/fr/univavignon/pokedex/api/IPokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/IPokemonFactory.java
@@ -1,11 +1,11 @@
-package fr.univavignon.pokedex.api;
+//package fr.univavignon.pokedex.api;
 
 /**
  * Factory interface for class that aims to create Pokemon instance.
  * 
  * @author fv
  */
-public interface IPokemonFactory {
+/*public interface IPokemonFactory {
 
 	/**
 	 * Creates a pokemon instance computing it IVs.
@@ -16,7 +16,69 @@ public interface IPokemonFactory {
 	 * @param dust Required dust for upgrading pokemon.
 	 * @param candy Required candy for upgrading pokemon.
 	 * @return Created pokemon instance.
-	 */
+	 *//*
 	Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) throws PokedexException;
 	
+}*/
+
+package fr.univavignon.pokedex.imp;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.apache.commons.collections4.map.UnmodifiableMap;
+
+import fr.univavignon.pokedex.api.IPokemonFactory;
+import fr.univavignon.pokedex.api.Pokemon;
+
+public class RocketPokemonFactory implements IPokemonFactory {
+	
+	private static Map<Integer, String> index2name;
+	static {
+		Map<Integer, String> aMap = new HashMap<Integer, String>();
+        aMap.put(-1, "Ash's Pikachu");
+        aMap.put(0, "MISSINGNO");
+        aMap.put(1, "Bulbasaur");
+        //TODO : Gotta map them all !
+        index2name = UnmodifiableMap.unmodifiableMap(aMap);
+	}
+	
+	private static int generateRandomStat() {
+		int total = 0;
+		for(int i=0; i < 1000000; i++)
+		{
+			Random rn = new Random();
+		    int r = rn.nextInt(2);
+		    total = total + r;
+		}
+		return total / 10000;
+	}
+
+	@Override
+	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+		String name;
+		if(!index2name.containsKey(index)) {
+			name = index2name.get(0);
+		} else {
+			name = index2name.get(index);
+		}
+		int attack;
+		int defense;
+		int stamina;
+		double iv;
+		if(index < 0) {
+			attack = 1000;
+			defense = 1000;
+			stamina = 1000;
+			iv = 0;
+		} else {
+			attack = RocketPokemonFactory.generateRandomStat();
+			defense = RocketPokemonFactory.generateRandomStat();
+			stamina = RocketPokemonFactory.generateRandomStat();
+			iv = 1;
+		}
+		return new Pokemon(index, name, attack, defense, stamina, cp, hp, dust, candy, iv);
+	}
+
 }
