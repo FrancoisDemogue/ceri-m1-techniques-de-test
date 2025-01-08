4051d3a61ffda6c7552fdb0fad0b199c714e75e9
Francois <147613612+FrancoisDemogue@users.noreply.github.com>
2025-01-08 08:33:39 +0100

    Update PokemonMetadataProvider.java

--- src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
@@ -32,7 +32,7 @@ public class PokemonMetadataProvider implements IPokemonMetadataProvider {
                 this.metadataCache.put(index, metadata);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new PokedexException("Error reading metadata file", e);
         }
     }
 
